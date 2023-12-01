04337fdd81d23a341052594d8063f431b2d8cfa0
Ayush-Manware <ayushmanware19@gmail.com>
2023-12-02 01:06:21 +0530

    Completed

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
+const jwt = require("jsonwebtoken")
 
 const Port = 2400;
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(cors({ origin: "*" }));
 
 const users = [];
@@ -20,16 +20,14 @@ app.get("/", (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { name, phone, email, password } = req.body;
-  console.log(req.body)
+  console.log(req.body);
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-
     users.push({ name, phone, email, password: hashedPassword });
-
     res.status(201).json({ messege: "User Registered !!" });
   } catch (error) {
     console.error("Error hashing password:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.json({ error: "Internal Server Error" });
   }
 });
 
@@ -37,18 +35,13 @@ app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
   const user = users.find((u) => u.email === email);
-  const findPswd = users.find((u)=> u.password === password)
-
-  if((user) && (findPswd)){
-    console.log("Login Matched")
-  }
 
   if (!user || !bcrypt.compareSync(password, user.password)) {
-    return res.status(401).json({ error: "User is not registered" });
+    return res.json({ error: "User is not registered" });
   }
 
   const token = jwt.sign({ email: user.email }, "secretKey");
-
+  console.log(req.body);
   res.json({ message: "User has logged in successfully", token });
 });
 
@@ -56,5 +49,3 @@ app.listen(Port, () => {
   console.log(users);
   console.log("Server Started");
 });
-
-
